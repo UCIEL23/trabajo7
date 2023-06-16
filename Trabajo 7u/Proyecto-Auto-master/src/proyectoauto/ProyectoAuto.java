7510b746eca1bea1947e302cd3b2e748f50aaf4d
UCIEL23 <132796971+UCIEL23@users.noreply.github.com>
2023-06-15 22:28:52 -0600

    Actualizar ProyectoAuto.java

--- Trabajo 7u/Proyecto-Auto-master/src/proyectoauto/ProyectoAuto.java
+++ Trabajo 7u/Proyecto-Auto-master/src/proyectoauto/ProyectoAuto.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package proyectoauto;
 
 import Principal.Auto;
@@ -11,7 +8,7 @@ import Principal.Deportivo;
 
 /**
  *
- * @author Ismasant
+ * @author Uciel 
  */
 public class ProyectoAuto {
 
