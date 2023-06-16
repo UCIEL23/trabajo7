10b4cbc35907bfe04287e9eabe9ad1be2680fa90
UCIEL23 <132796971+UCIEL23@users.noreply.github.com>
2023-06-15 22:31:25 -0600

    Actualizar Deportivo.java

--- Trabajo 7u/Proyecto-Auto-master/src/Principal/Deportivo.java
+++ Trabajo 7u/Proyecto-Auto-master/src/Principal/Deportivo.java
@@ -1,12 +1,10 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Principal;
 
 /**
  *
- * @author Ismasant
+ * @author Uciel 
  */
 public class Deportivo extends Auto{
     private String marca;
