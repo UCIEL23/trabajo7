dc13de256067a4564e2f36115f32bc31066eaa67
UCIEL23 <132796971+UCIEL23@users.noreply.github.com>
2023-06-15 22:29:24 -0600

    Actualizar Auto.java

--- Trabajo 7u/Proyecto-Auto-master/src/Principal/Auto.java
+++ Trabajo 7u/Proyecto-Auto-master/src/Principal/Auto.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Principal;
 
 /**
  *
- * @author Ismasante 
+ * @author Uciel 
  */
 public class Auto {
     protected String placa;
